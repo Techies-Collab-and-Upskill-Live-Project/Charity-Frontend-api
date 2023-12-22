80483bff7e7d9c650f80378b8b713cb32ee19991
Lomoee <favoursunday600@gmail.com>
2023-12-23 00:00:46 +0100

    favour commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import NavBar from './components/Navbar';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+
+import "./style.css";
+
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+         <NavBar />
+        <Routes>
+          <Route path="/"
+          // element={<Home />}
+          />
+          <Route path="/project"
+          // element={<Projects />}
+          />
+          <Route path="/about"
+          // element={<About />}
+          />
+          <Route path="/resume"
+          // element={<Resume />}
+          />
+          <Route path="*"
+          element={<Navigate to="/"/>}
+          />
+        </Routes>
+        {/* <Footer /> */}
+
+    </Router>
   );
 }
 
 export default App;
+
